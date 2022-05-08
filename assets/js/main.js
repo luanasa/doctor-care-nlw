1f547c19086e4adfb5f0044f5caa879e6aa038df
Luana Martins <38231334+luanasa@users.noreply.github.com>
2022-05-07 21:55:55 -0300

    finished the desktop view

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,41 @@ function onScroll() {
    showNavOnScroll()
    showBackToTopButtonOnScroll()
 
+    activeMenuAtCurrentSection(home)
+    activeMenuAtCurrentSection(services)
+    activeMenuAtCurrentSection(about)
+    activeMenuAtCurrentSection(contact)
+
 }
+
+function activeMenuAtCurrentSection(section) {
+    const targetLine = scrollY + innerHeight / 2
+
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+
+    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+    //console.log('section top reach or passed TargetLine?', sectionTopReachOrPassedTargetLine) // data info
+
+       const sectionEndsAt = sectionTop + sectionHeight
+       //console.log(sectionEndsAt)
+
+       const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+       //console.log('section end passed TargetLine?', sectionEndPassedTargetLine) // data info
+
+       const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+       const sectionId = section.getAttribute('id')
+       const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+       menuElement.classList.remove('active')
+        if (sectionBoundaries) {
+            menuElement.classList.add('active')
+    }
+}
+
+
 
 function showNavOnScroll() {
     if (scrollY > 0) {
@@ -37,3 +71,4 @@ ScrollReveal({
     distance: '30px',
     duration: 700,
 }).reveal('#home, #home img, #home .stats, #services, #services header, #services .card, #about, #about header, #about content');
+
